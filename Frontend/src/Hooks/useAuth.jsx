1f5bccb77c6f6680c68e350937201b161dbf4f9e
Adi-ops16 <hasibadi22@gmail.com>
2025-12-10 23:47:56 +0600

    git commit -m "seperated Frontend and Backend"

--- Frontend/src/Hooks/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Context/AuthContext';
-
-const useAuth = () => {
-    return useContext(AuthContext);
-};
-
-export default useAuth;
